9c0b6b43350f60371e66ed559f23802650d8eb52
Brad Taylor <btaylor@litl.com>
2012-06-27 15:17:07 -0400

    Initial Commit

---
+++ WKVerticalScrollBar/WKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WKAppDelegate.h
+//  WKVerticalScrollBar
+//
+//  Created by Brad Taylor on 6/27/12.
+//  Copyright (c) 2012 litl, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WKVerticalScrollBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WKVerticalScrollBar
+//
+//  Created by Brad Taylor on 6/27/12.
+//  Copyright (c) 2012 litl, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WKAppDelegate class]));
+    }
+}
